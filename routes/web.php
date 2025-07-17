8e5f4cd39a523e5fa4c5b6bbf788bd99def7c81c
oyi22 <oyinanjam.com>
2025-07-17 08:27:35 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -7,25 +7,19 @@ use App\Http\Controllers\AuthController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
+/// Halaman pertama langsung tampil login
+Route::get('/', function () {
+    return view('sign-in'); // ← ganti dari 'welcome' ke 'sign-in'
+})->name('sign-in');
 
-
-//registrasi
+/// Halaman registrasi
 Route::get('/sign-up', function () {
     return view('sign-up'); 
 })->name('sign-up');
+
 Route::post('/register', [AuthController::class, 'register'])->name('register');
-//login
-Route::get('/sign-in', function () {
-    return view('sign-in');
-});
+
+/// Login (form sudah dari /, jadi ini untuk proses login)
 Route::post('/login', [AuthController::class, 'login'])->name('login');
